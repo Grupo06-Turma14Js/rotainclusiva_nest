d33d5b35c80cfec8c0946bae4335d56c6dcbf945
Isabela <isabela_siqueirasilva@hotmail.com>
2026-04-13 15:43:26 -0300

    tratamento de erros

--- src/acessibilidade/dto/update-acessibilidade.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAcessibilidadeDto } from './create-acessibilidade.dto';
-
-export class UpdateAcessibilidadeDto extends PartialType(CreateAcessibilidadeDto) {}
